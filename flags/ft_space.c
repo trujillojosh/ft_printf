04e7c83515c83e6c6ae0909a9eac37b9a576c2f1
Josh Trujillo <jtrujill@e1z2r6p2.42.us.org>
2017-04-12 23:05:58 -0700

    added headers

--- flags/ft_space.c
+++ flags/ft_space.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_space.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtrujill <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/12 23:02:39 by jtrujill          #+#    #+#             */
+/*   Updated: 2017/04/12 23:02:42 by jtrujill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../ft_printf.h"
 
 int		ft_space(char **s1) /* if no sign is going to be written, a blank space is inserted before the value */
