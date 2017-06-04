50de84c32374d7dfbe434dfeb59e53af8920fbc8
Josh Trujillo <jtrujill@e1z2r6p2.42.us.org>
2017-06-03 18:07:09 -0700

    updated up x

--- conversions/ft_up_x.c
+++ conversions/ft_up_x.c
@@ -12,14 +12,41 @@
 
 #include "../ft_printf.h"
 
+static int	ft_zeroprec_upx(void *data, char *todo)
+{
+	if (data == NULL)
+	{
+		while ((*todo != '.') && (*todo != '\0'))
+			todo++;
+		if ((*todo == '.') && (*(todo + 1) != '\0'))
+		{
+			if ((*(todo + 1) == 'x') || (*(todo + 1) == '0'))
+			{
+				if (ft_isdigit(*(todo - 1)) == 1)
+					return (1);
+				else
+					return (-1);
+			}
+		}
+	}
+	return (0);
+}
+
 int		ft_up_x(va_list ap, char **s1, char *todo) /* Unsigned hexadecimal integer (uppercase) */ 
 {
 	void	*res;
 	char 	*tmp;
 
 	res = va_arg(ap, void *);
+	if ((ft_zeroprec_upx(res, todo) == 1) || (ft_zeroprec_upx(res, todo) == -1))
+	{
+		if (ft_zeroprec_upx(res, todo) == 1)
+			*s1 = ft_strinsert(*s1, " ", 1);
+		else
+			*s1 = ft_strinsert(*s1, "", 1);
+		return (0);
+	}
 	tmp = ft_itoa_dispatch(res, todo, 16);
-	// *s1 = ft_strinsert(*s1, ft_uitoa_base((unsigned int)res, 16), 1);
 	*s1 = ft_strinsert(*s1, tmp, 1);
 	ft_strdel(&tmp);
 	return (0);
