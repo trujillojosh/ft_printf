1ae2926f8e9f4e138dbfaff82330a6863f5a4b07
Josh Trujillo <jtrujill@e1z2r6p2.42.us.org>
2017-06-04 23:20:38 -0700

    a

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jtrujill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 22:46:21 by jtrujill          #+#    #+#             */
-/*   Updated: 2017/06/04 23:17:51 by jtrujill         ###   ########.fr       */
+/*   Updated: 2017/06/04 23:20:32 by jtrujill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int		ft_printf(const char *input, ...)
 
 	i = ft_char_count((char *)input, '%');
 	k = 0;
-	printf(">> %s\n", input);
+	write(1, input, ft_strlen(input));
 	if (i == 0)
 	{
 		ft_putstr(input);
