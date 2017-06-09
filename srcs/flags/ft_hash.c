18a75466d51bf16db63e016d088baa7c1225a1d2
Josh Trujillo <jtrujill@e1z2r6p2.42.us.org>
2017-06-08 18:58:32 -0700

    debugging...

--- srcs/flags/ft_hash.c
+++ srcs/flags/ft_hash.c
@@ -11,6 +11,19 @@
 /* ************************************************************************** */
 
 #include "../../includes/ft_printf.h"
+#include <stdio.h>
+
+static int hash_help(char *todo, int i)
+{
+	if ((i == 0) && (ft_tolower(todo[ft_strlen(todo) - 1]) == 'o'))
+	{
+		if (todo[ft_strlen(todo) - 2] == '.')
+			return (1);
+		else if ((todo[ft_strlen(todo) - 2] == '0') && (todo[ft_strlen(todo) - 3] == '.'))
+			return (1);
+	}
+	return (0);
+}
 
 int		ft_hash(char *str, char *todo, int start)
 {
@@ -30,9 +43,14 @@ int		ft_hash(char *str, char *todo, int start)
 		while (str[start] == ' ')
 			start++;
 		i = ft_atoi(&str[start]);
-		if ((i == 0) && (ft_tolower(todo[ft_strlen(todo) - 1]) == 'o'))
+		if (hash_help(todo, i) == 0)
 			return (0);
-		// if ((todo[ft_strlen(todo) - 1] == 'o') && (str[0] == '0'))
+		// if ((i == 0) && (ft_tolower(todo[ft_strlen(todo) - 1]) == 'o'))
+		// {
+		// 	if ((todo[ft_strlen(todo) - 2] == '.') || ((todo[ft_strlen(todo) - 2] == '0') && (todo[ft_strlen(todo) - 3] == '.')))
+		// 		return (0);
+		// }
+		// if ((todo[ft_strlen(todo) - 1] == 'o') && (str[0] == '0') && ((todo[ft_strlen(todo) - 2] == '.') || (todo[ft_strlen(todo) - 2] == '0')))
 		// 	return (0);
 		tmp = ft_strncpy(ft_strnew(start), (const char *)str, start);
 		tmp2 = ft_strncpy(ft_strnew(ft_strlen(&str[start])), (const char *)&str[start], (ft_strlen(&str[start])));
